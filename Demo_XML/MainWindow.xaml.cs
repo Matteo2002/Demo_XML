b454405d453c802d8be6ac802d8fc20ea46b7b05
Matteo Uras <Ursambriaco@hotmail.com>
2020-03-16 17:58:01 +0100

    Creazione progeto DemoXML

--- Demo_XML/MainWindow.xaml.cs
+++ Demo_XML/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Demo_XML
 {
@@ -23,6 +25,52 @@ namespace Demo_XML
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        bool flag = false;
+
+        private void Btn_Aggiorna_Click(object sender, RoutedEventArgs e)
+        {
+            Lst_Persone.Items.Clear();
+            flag = true;
+            Task.Factory.StartNew(() => CaricaDati());
+        }
+
+        private void CaricaDati()
+        {
+            Persona persona = new Persona();
+            string path = @"Documento.xml";
+            XDocument xmlDoc = XDocument.Load(path);
+            XElement xmlPersone = xmlDoc.Element("persone");
+            var xmlPersona = xmlPersone.Elements("persona");
+
+            foreach(var item in xmlPersona)
+            {
+                XElement xmlNome = item.Element("nome");
+                XElement xmlCognome = item.Element("cognome");
+                XElement xmlEta = item.Element("eta");
+                XElement xmlDataNascita = item.Element("data");
+
+                Persona p = new Persona();
+                p.Nome = xmlNome.Value;
+                p.Cognome = xmlCognome.Value;
+                p.Eta = Convert.ToInt32(xmlEta.Value);
+                p.DataNascita = Convert.ToDateTime(xmlDataNascita.Value);
+                persona = p;
+
+                Dispatcher.Invoke(() => Lst_Persone.Items.Add(persona));
+                Thread.Sleep(300);
+                if (!flag)
+                {
+                    break;
+                }
+
+            }
+        }
+
+        private void Btn_Stop_Click(object sender, RoutedEventArgs e)
+        {
+            flag = false;
         }
     }
 }
---
+++ Demo_XML/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_XML
+{
+    public class Persona
+    {
+        public string Nome { get; set; }
+
+        public string Cognome { get; set; }
+
+        public int Eta { get; set; }
+
+        public DateTime DataNascita { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nome} {Cognome}";
+        }
+    }
+}
